2918de19bcbe6e6b0f5756be7ce1c9f34a7c8c42
renatomno <xrenatomno@gmail.com>
2022-11-07 23:01:44 -0300

    add cpf validator

--- finAPI/src/index.js
+++ finAPI/src/index.js
@@ -12,10 +12,15 @@ app.get('/account', (request, response) => {
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "customer already exists" })
+  }
 
   customers.push({
-    id,
+    id: uuidv4(),
     cpf,
     name,
     'statement': []
