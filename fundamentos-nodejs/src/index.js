622ece6405613e71d363da72be53ab7ed8e93443
renatomno <xrenatomno@gmail.com>
2022-11-07 22:34:33 -0300

    :sparkles: http verbs examples

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'it works!' });
+app.use(express.json())
+
+const courses = [
+  { "curso": "nodejs" },
+  { "curso": "reactjs" },
+  { "curso": "elixir" }
+]
+
+app.get('/courses', (request, response) => {
+  return response.json(courses);
 })
+
+// app.post('/courses', (request, response) => {})
+// app.patch('/courses', (request, response) => {})
+// app.put('/courses', (request, response) => {})
+// app.delete('/courses', (request, response) => {})
 
 app.listen(3333, () => console.log('Servidor ouvindo a porta 3333'));
