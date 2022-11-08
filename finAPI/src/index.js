6c531809fd0fd0fc8926b515a77cd9b02c865e9a
renatomno <xrenatomno@gmail.com>
2022-11-07 22:57:06 -0300

    novo projeto && cadastro de contas

---
+++ finAPI/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+
+app.use(express.json())
+
+const customers = [];
+
+app.get('/account', (request, response) => {
+  return response.json(customers)
+})
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    'statement': []
+  })
+
+  return response.status(200).send()
+})
+
+app.listen(3000)
